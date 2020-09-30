2cddc82a63b83085ccde25c24e0c7f84e30c4a14
Ivan Derlich <e@ivanderlich.com>
2020-09-30 19:04:04 -0300

    Presentation of structure

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Button.css';
 
-function Button() {
+function Button({ buttonName }) {
   return (
     <div className="Button" id="button">
-      <p> Calculator App: Hello world!</p>
+      <p>
+        { buttonName}
+      </p>
     </div>
   );
 }
+
+Button.defaultProps = {
+  buttonName: 'defaultName',
+};
+
+Button.propTypes = {
+  buttonName: PropTypes.string,
+};
 
 export default Button;
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Display.css';
 
-function Display() {
+function Display({ result }) {
   return (
-    <div className="Display" id="button-panel">
-      <p> Calculator App: Hello world!</p>
+    <div className="Display" id="button-panel">      
+      <p>
+        Display Component:
+        {result}
+      </p>
     </div>
   );
 }
+
+Display.defaultProps = {
+  result: '0',
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
 
 export default Display;

89dc8c1af4fea5b8d5703fafc7e298c17a9d0293
Ivan Derlich <e@ivanderlich.com>
2020-10-06 17:40:01 -0300

    Edit

--- src/components/App.js
+++ src/components/App.js
@@ -5,24 +5,13 @@ import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
-
-  render() {
-    return (
-      <div className="App" id="app">
-        <Display />
-        <ButtonPanel />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App" id="app">
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
 }
 
 export default App;
